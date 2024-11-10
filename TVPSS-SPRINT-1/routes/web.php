969d4169bc18d1e946ed26dede02dc995f102dfc
hdaniel02 <hdanielreputationtv@gmail.com>
2024-11-11 03:37:53 +0800

    update web.php

--- TVPSS-SPRINT-1/routes/web.php
+++ TVPSS-SPRINT-1/routes/web.php
@@ -18,15 +18,30 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/studentLogin', [StudentController::class, 'studentLogin']);
+// Route::get('/studentLogin', [StudentController::class, 'studentLogin']);
 Route::get('/studentLogin', function () {
-    return Inertia::render('Students/LoginStudent');
+    return Inertia::render('5-Students/Auth/Login');
 });
 
+// Dashboard
 Route::get('/dashboard', function () {      
     return Inertia::render('1-SuperAdmin/SuperAdminDashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/dashboardState', function () {      
+    return Inertia::render('2-StateAdmin/StateAdminDashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/dashboardPPD', function () {      
+    return Inertia::render('3-PPDAdmin/PPDAdminDashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/dashboardSchool', function () {      
+    return Inertia::render('4-SchoolAdmin/SchoolAdminDashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+// profile
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
