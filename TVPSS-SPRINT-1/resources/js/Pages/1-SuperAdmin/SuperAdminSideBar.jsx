bd12a0b07ee66c23c3bcc781d019de0d89c13f04
syednopal <110806810+syednopal@users.noreply.github.com>
2024-11-11 00:30:58 +0800

    Update SuperAdminSideBar.jsx

--- TVPSS-SPRINT-1/resources/js/Pages/1-SuperAdmin/SuperAdminSideBar.jsx
+++ TVPSS-SPRINT-1/resources/js/Pages/1-SuperAdmin/SuperAdminSideBar.jsx
@@ -1,37 +1,39 @@
-import { FaTh, FaUsers, FaCog, FaSignOutAlt } from 'react-icons/fa';
+import React from 'react';
+import { FaCog, FaSignOutAlt, FaTh, FaUsers } from 'react-icons/fa';
 
-export default function SuperAdminSideBar() {
-    return (
-        <div className="w-1/8 bg-white text-gray-400 min-h-screen p-4 border-r border-gray-200 fixed">
-            {/* Logo Section */}
-            <div className="mb-6 text-center">
-                <img src="/public/assets/tvpsslogo.svg" alt="TVPSS Logo" className="mx-auto mb-2 w-24 h-24" />
-                <p className="text-sm">Management Information System</p>
-            </div>
+function SuperAdminSideBar() {
+  return (
+    <div className="fixed top-0 left-0 w-1/16 bg-gray-100 text-gray-800 h-screen p-4 border-r border-gray-300 flex flex-col justify-between">
+      {/* Logo Section */}
+      <div className="mb-2 text-center">
+        <img src="/assets/LogoTVPSS.svg" alt="TVPSS Logo" className="mx-auto w-100 h-100" />
+      </div>
 
-            {/* Menu Items */}
-            <ul className="space-y-2">
-                <li className="flex items-center space-x-2 px-4 py-2 text-gray-300 hover:text-gray-800 text-lg">
-                    <FaTh />
-                    <span>Dashboard</span>
-                </li>
-                <li className="flex items-center space-x-2 px-4 py-2 text-gray-300 hover:text-gray-800 text-lg">
-                    <FaUsers />
-                    <span>Pengurusan Pengguna</span>
-                </li>
-            </ul>
+      {/* Menu Items */}
+      <ul className="space-y-2">
+        <li className="flex items-center space-x-2 px-4 py-2 text-gray-600 hover:bg-gray-200 rounded-md text-lg">
+          <FaTh />
+          <span>Dashboard</span>
+        </li>
+        <li className="flex items-center space-x-2 px-4 py-2 text-gray-600 hover:bg-gray-200 rounded-md text-lg">
+          <FaUsers />
+          <span>Pengurusan Pengguna</span>
+        </li>
+      </ul>
 
-            {/* Bottom Items */}
-            <div className="mt-4 space-y-4">
-                <li className="flex items-center space-x-2 px-4 py-2 bg-[#455185] text-white rounded-md text-lg">
-                    <FaCog />
-                    <span>Tetapan</span>
-                </li>
-                <li className="flex items-center space-x-2 px-4 py-2 text-gray-300 hover:text-gray-800 text-lg">
-                    <FaSignOutAlt />
-                    <span>Log Keluar</span>
-                </li>
-            </div>
-        </div>
-    );
+      {/* Bottom Items */}
+      <div className="mt-2 space-y-4">
+        <li className="flex items-center space-x-2 px-4 py-2 bg-[#455185] text-white hover:bg-blue-800 rounded-md text-lg">
+          <FaCog />
+          <span>Tetapan</span>
+        </li>
+        <li className="flex items-center space-x-2 px-4 py-2 text-gray-600 hover:bg-gray-200 rounded-md text-lg">
+          <FaSignOutAlt />
+          <span>Log Keluar</span>
+        </li>
+      </div>
+    </div>
+  );
 }
+
+export default SuperAdminSideBar;
