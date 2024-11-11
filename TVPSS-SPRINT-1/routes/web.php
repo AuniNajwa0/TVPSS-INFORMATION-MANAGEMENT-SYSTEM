f74fe12f77b6abd1528cd38f5d1e906cf92dfb60
hdaniel02 <hdanielreputationtv@gmail.com>
2024-11-12 04:33:22 +0800

    update web.php

--- TVPSS-SPRINT-1/routes/web.php
+++ TVPSS-SPRINT-1/routes/web.php
@@ -23,6 +23,35 @@ Route::get('/studentLogin', function () {
     return Inertia::render('5-Students/Auth/Login');
 });
 
+// Dashboard
+Route::get('/dashboard', function () {      
+    return Inertia::render('1-SuperAdmin/SuperAdminDashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/dashboardState', function () {      
+    return Inertia::render('2-StateAdmin/StateAdminDashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/dashboardPPD', function () {      
+    return Inertia::render('3-PPDAdmin/PPDAdminDashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/dashboardSchool', function () {      
+    return Inertia::render('4-SchoolAdmin/SchoolAdminDashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+// Super Admin
+Route::get('/listUser', function () {
+    return Inertia::render('1-SuperAdmin/UserManagement/ListUser');
+});
+Route::get('/addUser', function(){
+    return Inertia::render('1-SuperAdmin/UserManagement/AddUser');
+});
+Route::get('/updateUser', function(){
+    return Inertia::render('1-SuperAdmin/UserManagement/UpdateUser');
+});
+
 // profile
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -30,8 +59,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+
+
 require __DIR__.'/auth.php';
-require __DIR__.'/superAdminRoutes.php'; 
-require __DIR__.'/stateAdminRoutes.php'; 
-require __DIR__.'/ppdAdminRoutes.php'; 
-require __DIR__.'/schoolAdminRoutes.php'; 
