0a0c5db5f4e673701b1cf2ab11460026d6a3d30d
hdaniel02 <hdanielreputationtv@gmail.com>
2024-11-12 04:35:06 +0800

    update side bar super admin

--- TVPSS-SPRINT-1/resources/js/Pages/1-SuperAdmin/SuperAdminSideBar.jsx
+++ TVPSS-SPRINT-1/resources/js/Pages/1-SuperAdmin/SuperAdminSideBar.jsx
@@ -1,4 +1,10 @@
-<div className="fixed top-0 left-0 w-1/16 bg-gray-100 text-gray-800 h-screen p-4 border-r border-gray-300 flex flex-col">
+import React from 'react';
+import { FaCog, FaSignOutAlt, FaTh, FaUsers } from 'react-icons/fa';
+import { Link } from '@inertiajs/react';  // Inertia Link
+
+function SuperAdminSideBar() {
+  return (
+    <div className="fixed top-0 left-0 w-1/16 bg-gray-100 text-gray-800 h-screen p-4 border-r border-gray-300 flex flex-col">
       {/* Logo Section */}
       <div className="mb-2 text-center">
         <img src="/assets/LogoTVPSS.svg" alt="TVPSS Logo" className="mx-auto w-100 h-100" />
@@ -7,12 +13,16 @@
       {/* Menu Items */}
       <ul className="space-y-2">
         <li className="flex items-center space-x-2 px-4 py-2 bg-[#455185] text-white rounded-md text-lg">
-          <FaTh />
-          <span>Dashboard</span>
+          <Link href="/dashboard">  {/* Correctly using Inertia Link */}
+            <FaTh />
+            <span>Dashboard </span>
+          </Link>
         </li>
         <li className="flex items-center space-x-2 px-4 py-2 text-gray-600 hover:bg-gray-200 rounded-md text-lg">
-          <FaUsers />
-          <span>Pengurusan Pengguna</span>
+          <Link href="/listUser">  {/* Correctly using Inertia Link */}
+            <FaUsers />
+            <span>Pengurusan Pengguna</span>
+          </Link>
         </li>
       </ul>
 
@@ -28,3 +38,7 @@
         </li>
       </div>
     </div>
+  );
+}
+
+export default SuperAdminSideBar;
