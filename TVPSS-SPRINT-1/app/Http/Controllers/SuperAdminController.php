af15fa5eef2177b148dc8b7959220cf64313654b
hdaniel02 <hdanielreputationtv@gmail.com>
2024-11-12 04:33:54 +0800

    update super admin controller

--- TVPSS-SPRINT-1/app/Http/Controllers/SuperAdminController.php
+++ TVPSS-SPRINT-1/app/Http/Controllers/SuperAdminController.php
@@ -12,5 +12,14 @@ class SuperAdminController extends Controller
     {
         return Inertia::render('1-SuperAdmin/UserManagement/ListUser');
     }
+
+    public function addUser()
+    {
+        return Inertia::render('1-SuperAdmin/UserManagement/AddUser');
+    }
+
+    public function updateUser()
+    {
+        return Inertia::render('1-SuperAdmin/UserManagement/UpdateUser');
+    }
 }
- 
