c6e3f9c6c4538638313a12c99eaa734b99bdae00
hdaniel02 <hdanielreputationtv@gmail.com>
2024-11-12 04:34:30 +0800

    update list user dh siap

--- TVPSS-SPRINT-1/resources/js/Pages/1-SuperAdmin/UserManagement/ListUser.jsx
+++ TVPSS-SPRINT-1/resources/js/Pages/1-SuperAdmin/UserManagement/ListUser.jsx
@@ -1,143 +1,214 @@
-import React, { useState } from 'react';
-import { FaEdit, FaTrash, FaFilter } from 'react-icons/fa';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
-
-
-const users = [
-    { id: 1, name: "Ahmad bin Mohamad", email: "ahmad.mohamad@moe-dl.edu.my", state: "Perlis", role: "SUPER ADMIN" },
-    { id: 2, name: "Chong Wei Jie", email: "chong.wei.jie@moe-dl.edu.my", state: "Terengganu", role: "ADMIN PPD" },
-    { id: 3, name: "Emily Davis", email: "emily.davis@moe-dl.edu.my", state: "Perak", role: "ADMIN SEKOLAH" },
-    { id: 4, name: "Jane Smith", email: "jane.smith@moe-dl.edu.my", state: "Selangor", role: "ADMIN STATE" },
-    { id: 5, name: "John Doe", email: "john.doe@moe-dl.edu.my", state: "Johor", role: "SUPER ADMIN" },
-    { id: 6, name: "Lee Siew Yen", email: "lee.siew.yen@moe-dl.edu.my", state: "Sabah", role: "ADMIN SEKOLAH" },
-    { id: 7, name: "Lim Chee Seng", email: "lim.chee.seng@moe-dl.edu.my", state: "Sabah", role: "ADMIN PPD" },
-    { id: 8, name: "Michael Johnson", email: "michael.johnson@moe-dl.edu.my", state: "Penang", role: "ADMIN PPD" },
-];
-
-const roles = ["Semua", "SUPER ADMIN", "ADMIN STATE", "ADMIN PPD", "ADMIN SEKOLAH"];
+import { FaFilter, FaEdit, FaTrash } from 'react-icons/fa';
+import SuperAdminSideBar from './SuperAdminSideBar';
+import { useState } from 'react';
 
 export default function Dashboard() {
-    const [filterRole, setFilterRole] = useState("Semua");
-    const [isDropdownOpen, setDropdownOpen] = useState(false);
+    // Dummy data (Replace with real data)
+    const allUsers = [
+        { id: 1, name: "Ahmad bin Mohamad", email: "ahmad.mohamad@moe-dl.edu.my", state: "Perlis", role: "SUPER ADMIN" },
+        { id: 2, name: "Chong Wei Jie", email: "chong.wei.jie@moe-dl.edu.my", state: "Terengganu", role: "ADMIN PPD" },
+        { id: 3, name: "Emily Davis", email: "emily.davis@moe-dl.edu.my", state: "Perak", role: "ADMIN SEKOLAH" },
+        { id: 4, name: "Jane Smith", email: "jane.smith@moe-dl.edu.my", state: "Selangor", role: "ADMIN STATE" },
+        { id: 5, name: "John Doe", email: "john.doe@moe-dl.edu.my", state: "Johor", role: "SUPER ADMIN" },
+        { id: 6, name: "John Doe", email: "john.doe@moe-dl.edu.my", state: "Johor", role: "SUPER ADMIN" },
+        { id: 7, name: "Alice Williams", email: "alice.williams@moe-dl.edu.my", state: "Kedah", role: "ADMIN PPD" },
+        { id: 8, name: "Bob Martin", email: "bob.martin@moe-dl.edu.my", state: "Penang", role: "ADMIN SEKOLAH" },
+        { id: 9, name: "Charlie Brown", email: "charlie.brown@moe-dl.edu.my", state: "Melaka", role: "ADMIN STATE" },
+        { id: 10, name: "Diana Prince", email: "diana.prince@moe-dl.edu.my", state: "Sarawak", role: "SUPER ADMIN" },
+        { id: 11, name: "Peter Parker", email: "peter.parker@moe-dl.edu.my", state: "Sabah", role: "ADMIN PPD" },
+    ];
 
-    // Filter users based on selected role
-    const filteredUsers = filterRole === "Semua"
-        ? users
-        : users.filter(user => user.role === filterRole);
+    // State variables
+    const [rowsPerPage, setRowsPerPage] = useState(10);  // Default rows per page
+    const [currentPage, setCurrentPage] = useState(1);
+    const [selectedRole, setSelectedRole] = useState('');  // New state for selected role
 
-    const toggleDropdown = () => {
-        setDropdownOpen(!isDropdownOpen);
+    // Filter users by role
+    const filteredUsers = selectedRole 
+        ? allUsers.filter(user => user.role === selectedRole)
+        : allUsers;
+
+    // Calculate the data to display
+    const indexOfLastUser = currentPage * rowsPerPage;
+    const indexOfFirstUser = indexOfLastUser - rowsPerPage;
+    const currentUsers = filteredUsers.slice(indexOfFirstUser, indexOfLastUser);
+
+    // Handle page change
+    const nextPage = () => {
+        if (currentPage < Math.ceil(filteredUsers.length / rowsPerPage)) {
+            setCurrentPage(currentPage + 1);
+        }
     };
 
-    const handleRoleSelect = (role) => {
-        setFilterRole(role);
-        setDropdownOpen(false);
+    const prevPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
+
+    // Handle rows per page change
+    const handleRowsPerPageChange = (e) => {
+        setRowsPerPage(Number(e.target.value));
+        setCurrentPage(1);  // Reset to first page when rows per page changes
+    };
+
+    // Handle role filter change
+    const handleRoleChange = (e) => {
+        setSelectedRole(e.target.value);
+        setCurrentPage(1);  // Reset to first page when role filter changes
     };
 
     return (
         <AuthenticatedLayout
             header={
                 <h2 className="text-xl font-semibold leading-tight text-gray-800">
-                    Semua Pengguna
+                    Pengurusan Pengguna
                 </h2>
             }
         >
-            <Head title="Dashboard" />
+            <Head title="Pengurusan Pengguna" />
 
-            <div className="p-6">
-                <div className="flex items-center justify-between mb-6">
-                    <h2 className="text-xl font-semibold text-gray-800">Pengurusan Pengguna Semua Pengguna</h2>
-                    <button className="bg-[#455185] text-white px-4 py-2 rounded-md hover:bg-[#3C4565]">
-                        Tambah Pengguna Baharu
-                    </button>
+            <div className="flex">
+                {/* Sidebar */}
+                <div className="w-1/6 p-8 text-white min-h-screen">
+                    <SuperAdminSideBar />
                 </div>
 
-                <div className="bg-white p-4 rounded-md shadow">
-                    <div className="flex items-center mb-4">
-                        <input
-                            type="text"
-                            placeholder="Cari Pengguna..."
-                            className="border rounded-md p-2 flex-grow"
-                        />
-                        <div className="relative ml-4">
-                            <button
-                                onClick={toggleDropdown}
-                                className="flex items-center p-2 border rounded-md text-gray-700 hover:bg-gray-200"
-                            >
-                                Filter <FaFilter className="ml-2" />
-                            </button>
-                            {isDropdownOpen && (
-                                <div className="absolute right-0 mt-2 w-40 bg-white border rounded-md shadow-lg z-10">
-                                    {roles.map((role) => (
-                                        <button
-                                            key={role}
-                                            onClick={() => handleRoleSelect(role)}
-                                            className={`block px-4 py-2 text-left w-full text-gray-700 hover:bg-gray-100 ${
-                                                filterRole === role ? "font-bold bg-gray-200" : ""
-                                            }`}
-                                        >
-                                            {role}
-                                        </button>
-                                    ))}
-                                </div>
-                            )}
-                        </div>
+                {/* Main Content */}
+                <div className="flex-1 p-6">
+                    <div className="flex justify-between items-center mb-4">
+                        <h3 className="text-lg font-semibold">Semua Pengguna</h3>
+                        <button className="bg-[#455185] hover:bg-[#3C4565] text-white rounded-md px-4 py-2 shadow-md">
+                            Tambah Pengguna Baharu
+                        </button>
                     </div>
 
-                    <table className="w-full text-left border-collapse">
-                        <thead>
-                            <tr>
-                                <th className="py-2 border-b">Bil</th>
-                                <th className="py-2 border-b">Nama Penuh</th>
-                                <th className="py-2 border-b">Alamat Email</th>
-                                <th className="py-2 border-b">Negeri</th>
-                                <th className="py-2 border-b">Jenis Pengguna</th>
-                                <th className="py-2 border-b">Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {filteredUsers.map((user, index) => (
-                                <tr key={user.id} className="hover:bg-gray-50">
-                                    <td className="py-2 border-b">{index + 1}</td>
-                                    <td className="py-2 border-b">{user.name}</td>
-                                    <td className="py-2 border-b">{user.email}</td>
-                                    <td className="py-2 border-b">{user.state}</td>
-                                    <td className="py-2 border-b">
-                                        <span className={`px-2 py-1 rounded-full text-white ${getRoleColor(user.role)}`}>
-                                            {user.role}
-                                        </span>
-                                    </td>
-                                    <td className="py-2 border-b flex space-x-2">
-                                        <button className="text-blue-600 hover:text-blue-800">
-                                            <FaEdit />
-                                        </button>
-                                        <button className="text-red-600 hover:text-red-800">
-                                            <FaTrash />
-                                        </button>
-                                    </td>
+                    <div className="max-w-8xl mx-auto p-6 text-gray-900 bg-white rounded shadow-md">
+                        {/* Search */}
+                        <div className="flex items-center mb-4 justify-between">
+                            <div className="flex items-center space-x-2 w-full max-w-xs">
+                                {/* Search Icon */}
+                                <FaFilter className="text-gray-500 text-xl" />
+                                <input
+                                    type="text"
+                                    placeholder="Cari Pengguna..."
+                                    className="w-full px-4 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-all placeholder-gray-400"
+                                />
+                            </div>
+
+                            {/* Role Filter Dropdown */}
+                            <div className="flex items-center space-x-4">
+                                <label htmlFor="roleFilter" className="text-sm font-medium">Jenis Pengguna :</label>
+                                <select
+                                    id="roleFilter"
+                                    value={selectedRole}
+                                    onChange={handleRoleChange}
+                                    className="px-4 py-2 bg-white text-[#455185] rounded-md border border-[#455185] shadow-lg transition-all hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-[#3C4565] hover:ring-2"
+                                >
+                                    <option value="">Semua</option>
+                                    <option value="SUPER ADMIN">Super Admin</option>
+                                    <option value="ADMIN PPD">PPD Admin</option>
+                                    <option value="ADMIN SEKOLAH">School Admin</option>
+                                    <option value="ADMIN STATE">State Admin</option>
+                                </select>
+
+                                <label htmlFor="rowsPerPage" className="text-sm font-medium">Bilangan Data :</label>
+                                <select
+                                    id="rowsPerPage"
+                                    value={rowsPerPage}
+                                    onChange={handleRowsPerPageChange}
+                                    className="px-7 py-2 bg-white text-[#455185] rounded-md border border-[#455185] shadow-lg transition-all hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-[#3C4565] hover:ring-2"
+                                >
+                                    <option value="5">5</option>
+                                    <option value="10">10</option>
+                                    <option value="25">25</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        {/* User Table */}
+                        <table className="w-full text-left border-collapse">
+                            <thead>
+                                <tr className="bg-gray-100">
+                                    <th className="border-b p-4">Bil</th>
+                                    <th className="border-b p-4">Nama Penuh</th>
+                                    <th className="border-b p-4">Alamat Email</th>
+                                    <th className="border-b p-4">Negeri</th>
+                                    <th className="border-b p-4">Jenis Pengguna</th>
+                                    <th className="border-b p-4">Aksi</th>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {currentUsers.map((user, index) => (
+                                    <tr key={user.id} className="hover:bg-gray-50">
+                                        <td className="border-b p-4">{index + 1 + indexOfFirstUser}</td>
+                                        <td className="border-b p-4">{user.name}</td>
+                                        <td className="border-b p-4">{user.email}</td>
+                                        <td className="border-b p-4">{user.state}</td>
+                                        <td className="border-b p-4">
+                                            <span className={`px-3 py-1 rounded-full text-xs ${getRoleColor(user.role)}`}>
+                                                {user.role}
+                                            </span>
+                                        </td>
+                                        <td className="border-b p-2">
+                                            <button className="mr-4 text-blue-600 hover:text-blue-800">
+                                                <FaEdit />
+                                            </button>
+                                            <button className="mr-4 text-red-600 hover:text-red-800">
+                                                <FaTrash />
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+
+                        {/* Pagination Section */}
+                        <div className="flex justify-center items-center mt-6 space-x-4">
+                            {/* Previous Page Button */}
+                            <button
+                                onClick={prevPage}
+                                className="p-2 bg-[#455185] text-white rounded-lg shadow-md hover:bg-[#3C4565] transition-colors duration-300 disabled:bg-gray-300 disabled:cursor-not-allowed"
+                                disabled={currentPage === 1}
+                            >
+                                <span className="text-xl">&lt;</span>
+                            </button>
+
+                            {/* Current Page Display */}
+                            <span className="text-sm text-gray-600 font-semibold">
+                                Halaman {currentPage} daripada {Math.ceil(filteredUsers.length / rowsPerPage)}
+                            </span>
+
+                            {/* Next Page Button */}
+                            <button
+                                onClick={nextPage}
+                                className="p-2 bg-[#455185] text-white rounded-lg shadow-md hover:bg-[#3C4565] transition-colors duration-300 disabled:bg-gray-300 disabled:cursor-not-allowed"
+                                disabled={currentPage === Math.ceil(filteredUsers.length / rowsPerPage)}
+                            >
+                                <span className="text-xl">&gt;</span>
+                            </button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </AuthenticatedLayout>
     );
 }
 
-// Helper function to set role color
-const getRoleColor = (role) => {
+// Helper function to assign colors based on role
+function getRoleColor(role) {
     switch (role) {
-        case 'SUPER ADMIN':
-            return 'bg-purple-500';
-        case 'ADMIN PPD':
-            return 'bg-blue-300';
-        case 'ADMIN SEKOLAH':
-            return 'bg-purple-300';
-        case 'ADMIN STATE':
-            return 'bg-yellow-300';
+        case "SUPER ADMIN":
+            return "bg-purple-100 text-purple-900";
+        case "ADMIN PPD":
+            return "bg-pink-100 text-pink-900";
+        case "ADMIN SEKOLAH":
+            return "bg-yellow-100 text-yellow-900";
+        case "ADMIN STATE":
+            return "bg-green-100 text-green-900";
         default:
-            return 'bg-gray-300';
+            return "bg-gray-300 text-gray-600";
     }
-};
+}
