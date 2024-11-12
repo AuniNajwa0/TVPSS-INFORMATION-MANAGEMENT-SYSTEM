b926b8e2a5e48bd3bcdd58ef64442a4df7abb399
hdaniel02 <hdanielreputationtv@gmail.com>
2024-11-13 00:02:27 +0800

    update side bar school admin

---
+++ TVPSS-SPRINT-1/resources/js/Pages/4-SchoolAdmin/SchoolAdminSideBar.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { FaCheckCircle, FaCog, FaInfoCircle, FaPaperPlane, FaPencilAlt, FaSignOutAlt, FaSortNumericDown, FaTh, FaUsers } from 'react-icons/fa';
+
+function SchoolAdminSideBar() {
+  return (
+    <div className="fixed top-0 left-0 w-1/16 bg-gray-100 text-gray-800 h-screen p-4 border-r border-gray-300 flex flex-col justify-between">
+      {/* Logo Section */}
+      <div className="mb-2 text-center">
+        <img src="/assets/LogoTVPSS.svg" alt="TVPSS Logo" className="mx-auto w-100 h-100" />
+      </div>
+
+      {/* Menu Items */}
+      <ul className="space-y-2">
+        <li className="flex items-center space-x-2 px-4 py-2 text-gray-600 hover:bg-gray-200 rounded-md text-lg">
+          <FaTh />
+          <span>Dashboard</span>
+        </li>
+        <li className="flex items-center space-x-2 px-4 py-2 text-gray-600 hover:bg-gray-200 rounded-md text-lg">
+          <FaPencilAlt />
+          <span>Permohonan Krew</span>
+        </li>
+        <li className="flex items-center space-x-2 px-4 py-2 text-gray-600 hover:bg-gray-200 rounded-md text-lg">
+          <FaCheckCircle />
+          <span>Pencapaian Pelajar</span>
+        </li>
+        <li className="flex items-center space-x-2 px-4 py-2 text-gray-600 hover:bg-gray-200 rounded-md text-lg">
+          <FaInfoCircle />
+          <span>Informasi Sekolah</span>
+        </li>
+        <li className="flex items-center space-x-2 px-4 py-2 text-gray-600 hover:bg-gray-200 rounded-md text-lg">
+          <FaSortNumericDown />
+          <span>Bilangan Barang</span>
+        </li>
+        <li className="flex items-center space-x-2 px-4 py-2 text-gray-600 hover:bg-gray-200 rounded-md text-lg">
+          <FaPaperPlane />
+          <span>Submit Versi TVPSS</span>
+        </li>
+      </ul>
+
+      {/* Bottom Items */}
+      <div className="mt-2 space-y-4">
+        <li className="flex items-center space-x-2 px-4 py-2 bg-[#455185] text-white hover:bg-blue-800 rounded-md text-lg">
+          <FaCog />
+          <span>Tetapan</span>
+        </li>
+        <li className="flex items-center space-x-2 px-4 py-2 text-gray-600 hover:bg-gray-200 rounded-md text-lg">
+          <FaSignOutAlt />
+          <span>Log Keluar</span>
+        </li>
+      </div>
+    </div>
+  );
+}
+
+export default SchoolAdminSideBar;
