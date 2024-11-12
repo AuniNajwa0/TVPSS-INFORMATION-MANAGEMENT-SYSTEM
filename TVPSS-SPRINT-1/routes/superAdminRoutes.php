e01b2eae5d8a07862572072a12b121f122bcce4b
hdaniel02 <hdanielreputationtv@gmail.com>
2024-11-12 23:15:52 +0800

    update super admin routes

--- TVPSS-SPRINT-1/routes/superAdminRoutes.php
+++ TVPSS-SPRINT-1/routes/superAdminRoutes.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\SuperAdminController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/dashboardSuper', function () {      
+Route::get('/dashboard', function () {      
     return Inertia::render('1-SuperAdmin/SuperAdminDashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
@@ -15,8 +15,13 @@ Route::get('/dashboardSuper', function () {
 Route::get('/SuperAdminDashboard', [SuperAdminController::class, 'SuperAdminDashboard'])
     ->middleware(['auth', 'verified'])->name('superADashControl');*/
 
-Route::get('/listUser', function () {
-    return Inertia::render('1-SuperAdmin/UserManagement/ListUser');
-});
-
+    Route::get('/listUser', function () {
+        return Inertia::render('1-SuperAdmin/UserManagement/ListUser');
+    });
+    Route::get('/addUser', function(){
+        return Inertia::render('1-SuperAdmin/UserManagement/AddUser');
+    });
+    Route::get('/updateUser', function(){
+        return Inertia::render('1-SuperAdmin/UserManagement/UpdateUser');
+    });
 
