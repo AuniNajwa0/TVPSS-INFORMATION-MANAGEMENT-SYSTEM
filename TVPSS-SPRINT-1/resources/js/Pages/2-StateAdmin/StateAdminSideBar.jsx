27434c0a955da77248949dfe8c6d8e2cf765092a
syednopal <110806810+syednopal@users.noreply.github.com>
2024-11-13 01:58:25 +0800

    Update StateAdminSideBar.jsx

--- TVPSS-SPRINT-1/resources/js/Pages/2-StateAdmin/StateAdminSideBar.jsx
+++ TVPSS-SPRINT-1/resources/js/Pages/2-StateAdmin/StateAdminSideBar.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
-import { FaCog, FaInfo, FaInfoCircle, FaPrint, FaSignOutAlt, FaTh, FaUsers } from 'react-icons/fa';
+import { FaCog, FaInfoCircle, FaPrint, FaSignOutAlt, FaTh } from 'react-icons/fa';
+import { Link } from '@inertiajs/react';  // Inertia Link
 
 function StateAdminSideBar() {
   return (
-    <div className="fixed top-0 left-0 w-1/16 bg-gray-100 text-gray-800 h-screen p-4 border-r border-gray-300 flex flex-col justify-between">
+    <div className="fixed top-0 left-0 w-1/6 bg-gray-100 text-gray-800 h-screen p-4 border-r border-gray-300 flex flex-col">
       {/* Logo Section */}
       <div className="mb-2 text-center">
-        <img src="/assets/LogoTVPSS.svg" alt="TVPSS Logo" className="mx-auto w-100 h-100" />
+        <img src="/assets/LogoTVPSS.svg" alt="TVPSS Logo" className="w-[100] h-[100]" />
       </div>
 
       {/* Menu Items */}
-      <ul className="space-y-2">
-        <li className="flex items-center space-x-2 px-4 py-2 text-gray-600 hover:bg-gray-200 rounded-md text-lg">
-          <FaTh />
-          <span>Dashboard</span>
+      <ul className="space-y-2 flex-1">
+        <li className="flex items-center space-x-2 px-4 py-2 bg-[#455185] text-white rounded-md text-lg">
+          <Link href="/dashboardState"> {/* Using Inertia Link */}
+            <FaTh />
+            <span>Dashboard</span>
+          </Link>
         </li>
         <li className="flex items-center space-x-2 px-4 py-2 text-gray-600 hover:bg-gray-200 rounded-md text-lg">
-          <FaPrint/>
-          <span>Jana Sijil Pelajar</span>
+          <Link href="/generateCertificates"> {/* Using Inertia Link */}
+            <FaPrint />
+            <span>Jana Sijil Pelajar</span>
+          </Link>
         </li>
-      <li className="flex items-center space-x-2 px-4 py-2 text-gray-600 hover:bg-gray-200 rounded-md text-lg">
-          <FaInfoCircle />
-          <span>Informasi TVPSS Sekolah</span>
+        <li className="flex items-center space-x-2 px-4 py-2 text-gray-600 hover:bg-gray-200 rounded-md text-lg">
+          <Link href="/schoolInfo"> {/* Using Inertia Link */}
+            <FaInfoCircle />
+            <span>Informasi TVPSS Sekolah</span>
+          </Link>
         </li>
       </ul>
 
@@ -33,7 +40,7 @@ function StateAdminSideBar() {
         </li>
         <li className="flex items-center space-x-2 px-4 py-2 text-gray-600 hover:bg-gray-200 rounded-md text-lg">
           <FaSignOutAlt />
-          <span>Lg Keluar</span>
+          <span>Log Keluar</span>
         </li>
       </div>
     </div>
