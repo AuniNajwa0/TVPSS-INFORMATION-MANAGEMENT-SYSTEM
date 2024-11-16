4576a25ab9db454c971bb19c9e3bb101863fb0cb
syednopal <110806810+syednopal@users.noreply.github.com>
2024-11-16 22:26:30 +0800

    Update schoolAdminRoutes.php

--- TVPSS-SPRINT-2/routes/schoolAdminRoutes.php
+++ TVPSS-SPRINT-2/routes/schoolAdminRoutes.php
@@ -8,6 +8,18 @@ Route::get('/dashboardSchool', function () {
     return Inertia::render('4-SchoolAdmin/SchoolAdminDashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/updateSchool', function () {      
+    return Inertia::render('4-SchoolAdmin/SchoolInformation/UpdateSchoolInformation');
+});
+
+Route::get('/updateSchoolTVPSSVersion', function () {      
+    return Inertia::render('4-SchoolAdmin/SchoolInformation/UpdateSchoolVersionStatus');
+});
+
+Route::get('/updateSchoolTVPSSVersion2', function () {      
+    return Inertia::render('4-SchoolAdmin/SchoolInformation/UpdateSchoolVersionStatus2');
+});
+
 /*Route::get('/SchoolAdminDashboard', function () {
     return Inertia::render('1-SchoolAdmin/SchoolAdminDashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
