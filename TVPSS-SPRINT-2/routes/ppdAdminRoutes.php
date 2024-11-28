23628d42f3af252d7c7080357657009129485cf8
Mnqarlz <76693751+mnqarlz@users.noreply.github.com>
2024-11-28 14:52:19 +0800

    Update ppdAdminRoutes.php

--- TVPSS-SPRINT-2/routes/ppdAdminRoutes.php
+++ TVPSS-SPRINT-2/routes/ppdAdminRoutes.php
@@ -4,13 +4,9 @@ use App\Http\Controllers\PPDAdminController;
 use Illuminate\Support\Facades\Route; 
 use Inertia\Inertia;
 
-Route::get('/dashboardPPD', function () {      
+/*Route::get('/dashboardPPD', function () {      
     return Inertia::render('3-PPDAdmin/PPDAdminDashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboardPP');*/
 
-/*Route::get('/PPDAdminDashboard', function () {      
-    return Inertia::render('3-PPDAdmin/PPDAdminDashboard');
-})->middleware(['auth', 'verified'])->name('ppdADashControl');
-
-Route::get('/PPDAdminDashboard', [PPDAdminController::class, 'SchoolAdminDashboard'])
-    ->middleware(['auth', 'verified'])->name('ppdADashControl');*/
+Route::get('/dashboardPPD', fn() => Inertia::render('3-PPDAdmin/PPDAdminDashboard'))
+    ->name('dashboardPP');
