951c908d024860236013858f66775af00a5efbae
Mnqarlz <76693751+mnqarlz@users.noreply.github.com>
2024-11-28 14:49:45 +0800

    Update superAdminRoutes.php

--- TVPSS-SPRINT-2/routes/superAdminRoutes.php
+++ TVPSS-SPRINT-2/routes/superAdminRoutes.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Models\User;
 
 // Route for Dashboard
 /*Route::get('/dashboardSuper', function () {
@@ -20,6 +21,10 @@ Route::post('/users', [UserController::class, 'store'])->name('users.store');
 Route::get('/updateUser/{user}', [UserController::class, 'edit'])->name('users.edit');
 Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
 Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+Route::get('/user-role-counts', [UserController::class, 'getUserRoleCounts']);
+Route::get('/user-role-counts/30-minutes', [UserController::class, 'getActiveUserCounts30Minutes']);
+
+
 
 Route::get('/profileSuperAdmin', function () {
     return Inertia::render('1-SuperAdmin/UserProfile/Edit');
