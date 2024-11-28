ad4f09f4da66e8fdc5a6a1202723ca0b6adac289
Mnqarlz <76693751+mnqarlz@users.noreply.github.com>
2024-11-28 14:52:55 +0800

    Update stateAdminRoutes.php

--- TVPSS-SPRINT-2/routes/stateAdminRoutes.php
+++ TVPSS-SPRINT-2/routes/stateAdminRoutes.php
@@ -4,13 +4,9 @@ use App\Http\Controllers\StateAdminController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/dashboardState', function () {      
+/*Route::get('/dashboardState', function () {      
     return Inertia::render('2-StateAdmin/StateAdminDashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboardST');*/
 
-/*Route::get('/StateAdminDashboard', function () {      
-    return Inertia::render('2-StateAdmin/StateAdminDashboard');
-})->middleware(['auth', 'verified'])->name('stateADashControl');
-
-Route::get('/StateAdminDashboard', [StateAdminController::class, 'StateAdminDashboard'])
-    ->middleware(['auth', 'verified'])->name('stateADashControl');*/
+Route::get('/dashboardState', fn() => Inertia::render('2-StateAdmin/StateAdminDashboard'))
+    ->name('dashboardST');
