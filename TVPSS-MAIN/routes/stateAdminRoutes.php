c944107853898f5ff8e748b497ce0fe8c05fef55
hdaniel02 <148690978+hdaniel02@users.noreply.github.com>
2024-12-15 09:55:18 +0800

    Update stateAdminRoutes.php

--- TVPSS-MAIN/routes/stateAdminRoutes.php
+++ TVPSS-MAIN/routes/stateAdminRoutes.php
@@ -20,3 +20,12 @@ Route::get('/certificate-templates/{id}', [StateAdminController::class, 'getTemp
 Route::put('/certificate-templates/{id}', [StateAdminController::class, 'updateTemplate']);
 Route::get('/certificate-templates/{id}/edit', [StateAdminController::class, 'editTemplate'])->name('certificate-templates.edit');
 
+Route::get('/listRequestCertificate',  fn() => Inertia::render('2-StateAdmin/StudentCertificate/CertificateRequestList'))
+->name('dashboardST');
+
+Route::get('/listGenerateCertificate',  fn() => Inertia::render('2-StateAdmin/StudentCertificate/CertificateGenerateList'))
+->name('dashboardST');
+
+Route::get('/generateCertificate',  fn() => Inertia::render('2-StateAdmin/StudentCertificate/GenerateCertificate'))
+->name('dashboardST');
+
