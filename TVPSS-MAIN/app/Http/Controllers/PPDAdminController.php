526596166677a3ae8a1ba59adaeeba347dfd6d66
Mnqarlz <aiman020404@gmail.com>
2024-12-21 23:49:07 +0800

    update ppd admin function to list out all school within district

--- TVPSS-MAIN/app/Http/Controllers/PPDAdminController.php
+++ TVPSS-MAIN/app/Http/Controllers/PPDAdminController.php
@@ -8,16 +8,25 @@ use Inertia\Inertia;
 
 class PPDAdminController extends Controller
 {
-    public function tvpssInfoIndex()
+    public function tvpssInfoPPDIndex(Request $request)
     {
-        $schoolInfo = SchoolInfo::first(); 
-        
+        $user = $request->user(); 
+
+        if (!$user->district) {
+            return Inertia::render('3-PPDAdmin/SchoolVersionStatus/listSchool', [
+                'schools' => [],
+                'message' => 'No schools found for your district.',
+            ]);
+        }
+
+        $schools = SchoolInfo::where('district', $user->district)->get();
+
         return Inertia::render('3-PPDAdmin/SchoolVersionStatus/listSchool', [
-            'schoolInfo' => $schoolInfo, 
+            'schools' => $schools,
         ]);
     }
 
-    public function tvpssInfoEdit(string $id)
+    public function tvpssInfoPPDEdit(string $id)
     {
         return Inertia::render('3-PPDAdmin/SchoolVersionStatus/listSchool');
     }
