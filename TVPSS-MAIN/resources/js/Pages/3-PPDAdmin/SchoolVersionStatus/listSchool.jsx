cda1675f0ec586f79e45a0b4fb96a9245507e292
mnqarlz <aiman020404@gmail.com>
2024-12-24 22:58:03 +0800

    update route

--- TVPSS-MAIN/resources/js/Pages/3-PPDAdmin/SchoolVersionStatus/listSchool.jsx
+++ TVPSS-MAIN/resources/js/Pages/3-PPDAdmin/SchoolVersionStatus/listSchool.jsx
@@ -214,7 +214,11 @@ export default function TVPSSInfoSchoolPPD({ schools }) {
                                         <td className="p-4">
                                             <button
                                                 className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-                                                onClick={() => router.visit(`/tvpssInfoPPD/${school.schoolCode}/edit`)}
+                                                onClick={() =>
+                                                    router.visit(
+                                                        `/tvpssInfoPPD/${school.schoolCode}/edit`
+                                                    )
+                                                }
                                             >
                                                 Lihat
                                             </button>
