1577ce052e86e9a769e793fac4532d9b0deaf2c0
mnqarlz <aiman020404@gmail.com>
2024-12-26 03:48:18 +0800

    add student model

---
+++ TVPSS-MAIN/app/Models/Student.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use MongoDB\Laravel\Eloquent\Model as Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'ic_num',
+        'email',
+        'crew',
+        'state',
+        'district',
+        'schoolName',
+        'school_info_id',
+    ];
+
+    /**
+     * Relationship with the SchoolInfo model.
+     * This links a student to their school.
+     */
+    public function schoolInfo()
+    {
+        return $this->belongsTo(SchoolInfo::class, 'school_info_id');
+    }
+}
