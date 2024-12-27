c9cf6477133527a161b1fb2c1ad124776a5e73e4
auniNajwa <auninajwa@graduate.utm.my>
2024-12-28 01:22:07 +0800

    model pulerks

--- TVPSS-MAIN/app/Models/Studcrew.php
+++ TVPSS-MAIN/app/Models/Studcrew.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use Illuminate\Database\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\Model as Model;
 
 class Studcrew extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'studcrew';
+
+    protected $guarded = [
+        'id',
+    ];
 }
