0bf8c9e52006a52a122c36ce1fd3f15458f75277
hdaniel02 <148690978+hdaniel02@users.noreply.github.com>
2024-12-29 23:41:47 +0800

    Update UpdateProfileInformationForm.jsx

--- TVPSS-MAIN/resources/js/Pages/1-SuperAdmin/UserProfile/Partials/UpdateProfileInformationForm.jsx
+++ TVPSS-MAIN/resources/js/Pages/1-SuperAdmin/UserProfile/Partials/UpdateProfileInformationForm.jsx
@@ -1,119 +1,60 @@
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import { Transition } from '@headlessui/react';
-import { Link, useForm, usePage } from '@inertiajs/react';
-
-export default function UpdateProfileInformation({
-    mustVerifyEmail,
-    status,
-    className = '',
-}) {
-    const user = usePage().props.auth.user;
-
-    const { data, setData, patch, errors, processing, recentlySuccessful } =
-        useForm({
-            name: user.name,
-            email: user.email,
-        });
-
-    const submit = (e) => {
-        e.preventDefault();
-
-        patch(route('profile.update'));
-    };
-
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head } from '@inertiajs/react';
+import DeleteUserForm from './Partials/DeleteUserForm';
+import UpdatePasswordForm from './Partials/UpdatePasswordForm';
+import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm';
+import SuperAdminSideBar from "../SuperAdminSideBar";
+export default function Edit({ mustVerifyEmail, status }) {
     return (
-        <section className={className}>
-            <header>
-                <h2 className="text-lg font-medium text-gray-900">
-                    Maklumat Peribadi
-                </h2>
+        <AuthenticatedLayout>
+            <Head title="TVPSS | Profile" />
 
-                <p className="mt-1 text-sm text-gray-600">
-                    Kemaskini maklumat profil dan email anda.
-                </p>
-            </header>
+    <div className="flex flex-col md:flex-row min-h-screen bg-white">
+    <div className="w-1/6 bg-white shadow-lg">
+    <SuperAdminSideBar />
+        </div>
 
-            <form onSubmit={submit} className="mt-6 space-y-6">
-                <div>
-                    <InputLabel htmlFor="name" value="Nama" />
-
-                    <TextInput
-                        id="name"
-                        className="mt-1 block w-full"
-                        value={data.name}
-                        onChange={(e) => setData('name', e.target.value)}
-                        required
-                        isFocused
-                        autoComplete="name"
-                    />
-
-                    <InputError className="mt-2" message={errors.name} />
-                </div>
-
-                <div>
-                    <InputLabel htmlFor="email" value="Email" />
-
-                    <TextInput
-                        id="email"
-                        type="email"
-                        className="mt-1 block w-full"
-                        value={data.email}
-                        onChange={(e) => setData('email', e.target.value)}
-                        required
-                        autoComplete="username"
-                    />
-
-                    <InputError className="mt-2" message={errors.email} />
-                </div>
-
-                {mustVerifyEmail && user.email_verified_at === null && (
-                    <div>
-                        <p className="mt-2 text-sm text-gray-800">
-                            Your email address is unverified.
-                            <Link
-                                href={route('verification.send')}
-                                method="post"
-                                as="button"
-                                className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                            >
-                                Click here to re-send the verification email.
-                            </Link>
-                        </p>
-
-                        {status === 'verification-link-sent' && (
-                            <div className="mt-2 text-sm font-medium text-green-600">
-                                A new verification link has been sent to your
-                                email address.
-                            </div>
-                        )}
+        <div className="w-full md:ml-[120px] p-6">
+        <div>
+                            <h2 className="text-3xl font-bold text-gray-900 bg-clip-text  ">
+                                <a
+                                    href="/profileSuperAdmin"
+                                    className="text-[#455185] hover:underline"
+                                >
+                                    Tetapan
+                                </a>
+                                <span className="mx-2 text-gray-500">{'>'}</span>
+                                <a
+                                    href="/profileSuperAdmin"
+                                    className="text-gray-700 "
+                                >
+                                    Kemas Kini Maklumat Peribadi
+                                </a>
+                            </h2>
+                        </div>
+            
+            <div className="p-12">
+                <div className="mx-auto max-w-4xl space-y-6 sm:px-6 lg:px-8">
+                    <div className="bg-white border p-4 shadow sm:rounded-lg sm:p-8">
+                        <UpdateProfileInformationForm
+                            mustVerifyEmail={mustVerifyEmail}
+                            status={status}
+                            className="max-w-xl"
+                        />
                     </div>
-                )}
 
-                <div className="flex items-center gap-4">
-                    <PrimaryButton 
-                                        style={{ backgroundColor: '#4158A6' }}
-                                        className="hover:bg-gray-700 text-white"
-                                        disabled={processing}
-                                        >
-                                        Simpan
-                                        </PrimaryButton>
+                    <div className="bg-white border p-4 shadow sm:rounded-lg sm:p-8">
+                        <UpdatePasswordForm className="max-w-xl" />
+                    </div>
 
-                    <Transition
-                        show={recentlySuccessful}
-                        enter="transition ease-in-out"
-                        enterFrom="opacity-0"
-                        leave="transition ease-in-out"
-                        leaveTo="opacity-0"
-                    >
-                        <p className="text-sm text-gray-600">
-                            Saved.
-                        </p>
-                    </Transition>
+                    <div className="bg-white border p-4 shadow sm:rounded-lg sm:p-8">
+                        <DeleteUserForm className="max-w-xl" />
+                    </div>
                 </div>
-            </form>
-        </section>
+            </div>
+            </div>
+            </div>
+            
+        </AuthenticatedLayout>
     );
 }
